c5b181518b0d8998d7d9ab4f687de16b97f10aa1
vladimir <okulovvs@gmail.com>
2020-10-18 23:11:32 +0300

    db

--- migrate.bat
+++ migrate.bat
@@ -1,7 +1,9 @@
 python manage.py makemigrations
 pause
 python manage.py migrate
-python manage.py createsuperuser
+
+
+
 
 python manage.py dumpdata > dupm.json
 python manage.py loaddata dupm.json
